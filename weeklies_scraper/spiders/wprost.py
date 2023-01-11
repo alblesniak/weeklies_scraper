e73d53bc0c6a555b97ae2bf658e6f90f418abcff
alblesniak <alblesniak@users.noreply.github.com>
2023-01-11 14:06:56 +0100

    retrying wprost

--- weeklies_scraper/spiders/wprost.py
+++ weeklies_scraper/spiders/wprost.py
@@ -17,16 +17,41 @@ class WprostSpider(scrapy.Spider):
 
     def __init__(self):
         chrome_options = Options()
-        chrome_options.add_argument("--disable-extensions")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument('--window-size=1420,1080')
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--accept_untrusted_certs")
-        chrome_options.add_argument("--accept_insecure_certs")
+        # chrome_options.add_argument("--disable-extensions")
+        # chrome_options.add_argument("--disable-gpu")
+        # chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument('--window-size=1420,1080')
+        # chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--accept_untrusted_certs")
+        # chrome_options.add_argument("--accept_insecure_certs")
         self.driver = webdriver.Chrome(options=chrome_options)
         self.username = os.environ.get('WEEKLIES_SCRAPER_LOGIN')
         self.password = os.environ.get('WEEKLIES_SCRAPER_PASSWORD')
+
+    def login(self, response):
+        self.driver.get(response.url)
+        username_element = self.driver.find_element(
+            "xpath", '//input[@id="username"]')
+        self.driver.execute_script(
+            "document.getElementById('username').click()")
+        self.driver.execute_script(
+            "arguments[0].setAttribute('value', '" + self.username + "')", username_element)
+        time.sleep(1)
+        password_element = self.driver.find_element(
+            "xpath", '//input[@id="password"]')
+        self.driver.execute_script(
+            "document.getElementById('password').click()")
+        self.driver.execute_script(
+            "arguments[0].setAttribute('value', '" + self.password + "')", password_element)
+        self.driver.execute_script("arguments[0].click();", password_element)
+        time.sleep(1)
+        button_element = self.driver.find_element(
+            "xpath", '//button[@class="button button-action"]')
+        self.driver.execute_script("arguments[0].click();", button_element)
+        self.logger.info("Login to the website...")
+        time.sleep(1)
+        # Store the cookies set by the website
+        self.cookies = self.driver.get_cookies()
 
     def check_login(self, root):
         try:
@@ -40,32 +65,18 @@ class WprostSpider(scrapy.Spider):
     def parse(self, response):
         self.logger.info(
             'Parse function called parse_issue on {}'.format(response.url))
-        self.driver.get(response.url)
-        time.sleep(3)
-        username_element = self.driver.find_element(
-            "xpath", '//input[@id="username"]')
-        self.driver.execute_script(
-            "document.getElementById('username').click()")
-        self.driver.execute_script(
-            "arguments[0].setAttribute('value', '" + self.username + "')", username_element)
-        time.sleep(3)
-        password_element = self.driver.find_element(
-            "xpath", '//input[@id="password"]')
-        self.driver.execute_script(
-            "document.getElementById('password').click()")
-        self.driver.execute_script(
-            "arguments[0].setAttribute('value', '" + self.password + "')", password_element)
-        self.driver.execute_script("arguments[0].click();", password_element)
-        time.sleep(3)
-        button_element = self.driver.find_element(
-            "xpath", '//button[@class="button button-action"]')
-        self.driver.execute_script("arguments[0].click();", button_element)
-        time.sleep(3)
+
+        # Log in to the website before making any requests
+        self.login(response=response)
         root = html.fromstring(self.driver.page_source)
-        years = root.xpath('.//ul[@class="years mtop10"]/li/a/@href')
-        for year in years:
-            year_url = urljoin(response.url, year)
-            yield response.follow(year_url, callback=self.parse_year)
+        # Check if login succeded
+        if self.check_login(root=root):
+            years = root.xpath('.//ul[@class="years mtop10"]/li/a/@href')
+            for year in years:
+                year_url = urljoin(response.url, year)
+                yield response.follow(year_url, callback=self.parse_year)
+        else:
+            self.logger.error(f'Could not to login to {response.url}')
 
     def parse_year(self, response):
         self.logger.info(
@@ -102,31 +113,32 @@ class WprostSpider(scrapy.Spider):
     def parse_article(self, response):
         self.logger.info(
             'Parse function called parse_article on {}'.format(response.url))
-
-        while True:
-            self.driver.get(response.url)
+        time.sleep(1)
+        root = html.fromstring(self.driver.page_source)
+        # Check if login succeded
+        if self.check_login(root=root):
+            loader = ItemLoader(item=WeekliesScraperItem(), response=response)
+            loader.add_value('issue_name', response.meta['issue_name'])
+            loader.add_value('issue_number', response.meta['issue_number'])
+            loader.add_value('issue_year', response.meta['issue_year'])
+            loader.add_value('issue_url', response.meta['issue_url'])
+            loader.add_value('issue_cover_url',
+                             response.meta['issue_cover_url'])
+            loader.add_value('article_authors', ', '.join(root.xpath(
+                '//article[@class="article"]/ul[@class="art-authors"]/li/a[@rel="author"]/strong/text()')))
+            loader.add_value('article_title', root.xpath(
+                '//h1[@class="art-title" or @class="art-title art-title-large"]/span/text()'))
+            loader.add_value('article_intro', root.xpath(
+                '//div[@id="art-lead-inner"]/text()'))
+            loader.add_value('article_content', root.xpath(
+                '//div[@id="art-text-inner"]//text()[not(ancestor::*[@class="art-text-frame"])]'))
+            loader.add_value('article_url', response.url)
+            yield loader.load_item()
+        else:
+            self.logger.error(f'Could not to login to {response.url}')
+            # Log in to the website before making any requests
+            self.login(response=response)
             time.sleep(3)
-            root = html.fromstring(self.driver.page_source)
-            if "Neuron" in str(self.check_login(root)):
-                break
-            else:
-                time.sleep(3)
-                pass
-        loader = ItemLoader(item=WeekliesScraperItem(), response=response)
-        loader.add_value('issue_name', response.meta['issue_name'])
-        loader.add_value('issue_number', response.meta['issue_number'])
-        loader.add_value('issue_year', response.meta['issue_year'])
-        loader.add_value('issue_url', response.meta['issue_url'])
-        loader.add_value('issue_cover_url', response.meta['issue_cover_url'])
-        loader.add_value('article_authors', ', '.join(root.xpath(
-            '//article[@class="article"]/ul[@class="art-authors"]/li/a[@rel="author"]/strong/text()')))
-        loader.add_value('article_title', root.xpath(
-            '//h1[@class="art-title" or @class="art-title art-title-large"]/span/text()')[0])
-        loader.add_value('article_intro', root.xpath(
-            '//div[@id="art-lead-inner"]/text()'))
-        loader.add_value('article_content', root.xpath(
-            '//div[@id="art-text-inner"]//text()[not(ancestor::*[@class="art-text-frame"])]'))
-        loader.add_value('article_url', response.url)
-        yield loader.load_item()
+            yield response.follow(response.url, callback=self.parse_article, meta=response.meta, cookies=self.cookies)
 
         # https://www.wprost.pl/tygodnik/10059385/Rzadowe-trzesienie-ziemi.html
